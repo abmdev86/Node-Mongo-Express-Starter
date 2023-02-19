867340b476709231638b3395303ec6125fd5378f
Auroiah Morgan <amwebdev@outlook.com>
2023-02-19 13:22:41 -0500

    Update router.js
    Adds post '/register' route

--- router.js
+++ router.js
@@ -1,16 +1,54 @@
-const express = require('express');
-
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("./db/userModel");
 const router = express.Router();
 
-
-
-router.use((req, res, next)=>{
-    console.log('Time: ', Date.now());
-    next();
-})
+router.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
 
 router.get("/", (req, res, next) => {
-  res.json({message: 'Hello from server!'});
+  res.json({ message: "Hello from server!" });
+});
+
+// register endpoint
+router.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
 module.exports = router;
