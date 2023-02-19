4cc53e5e9d66f1b13dc1c4478a798d7ab7f7d689
Auroiah Morgan <amwebdev@outlook.com>
2023-02-19 15:26:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 const http = require("http");
 const app = require("./app");
 
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || "3000");
-
-app.set("port", port);
-
 const errorHandler = (error) => {
   if (error.syscall !== "listen") {
     throw error;
@@ -37,7 +22,20 @@ const errorHandler = (error) => {
       throw error;
   }
 };
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+
+app.set("port", port);
 // create the server.
 const server = http.createServer(app);
 
